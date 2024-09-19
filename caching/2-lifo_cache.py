3204af652cbeef95441e3d68fb024e91744dc6f8
melekmoalla <melekmoalla2468@gmail.com>
2024-09-19 15:06:31 +0100

    test

--- caching/2-lifo_cache.py
+++ caching/2-lifo_cache.py
@@ -1,35 +1,46 @@
 #!/usr/bin/env python3
 
-""" LIFOCache defines:
-A class that inherits from BaseCaching and is a LIFO caching system
+""" BaseCaching defines:
+Create a class FIFOCache that inherits from
+BaseCaching and is a caching system:
 """
 
 from base_caching import BaseCaching
 
 
 class LIFOCache(BaseCaching):
-    """ LIFO Caching System """
+    """ BaseCaching defines:
+    Create a class FIFOCache that inherits from
+    BaseCaching and is a caching system:
+    """
 
     def __init__(self):
-        """ Initialize the cache """
+        """ function init
+        """
         super().__init__()
-        self.order = []  # List to keep track of the order of insertion
+        self.order = []
 
     def put(self, key, item):
-        """ Add an item to the cache """
+        """ function put
+        """
         if key and item:
             if key in self.cache_data:
-                self.order.remove(key)  # Remove key from order if it already exists
-
+                self.order.remove(key)
+                
             self.cache_data[key] = item
-            self.order.append(key)  # Add the key to the end of the order list
+            self.order.append(key)
 
-            # If cache exceeds the maximum number of items, discard the last added item
             if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-                last_key = self.order.pop(-2)  # Remove the second-last item (LIFO)
+                
+                last_key = list(self.cache_data)[3]
                 print(f"DISCARD: {last_key}")
+                ast_key = self.order.pop(-2)
                 del self.cache_data[last_key]
 
     def get(self, key):
-        """ Retrieve an item from the cache """
-        return self.cache_data.get(key, None)
+        """ function get
+        """
+        if key and key in self.cache_data:
+            return self.cache_data[key]
+        else:
+            return None
