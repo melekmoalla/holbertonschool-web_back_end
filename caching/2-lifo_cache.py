a693acc25a090ca4b4adea25f2a08bc46e6491d1
melekmoalla <melekmoalla2468@gmail.com>
2024-09-19 14:59:11 +0100

    test

--- caching/2-lifo_cache.py
+++ caching/2-lifo_cache.py
@@ -1,40 +1,35 @@
 #!/usr/bin/env python3
 
-""" BaseCaching defines:
-Create a class FIFOCache that inherits from
-BaseCaching and is a caching system:
+""" LIFOCache defines:
+A class that inherits from BaseCaching and is a LIFO caching system
 """
 
 from base_caching import BaseCaching
 
 
 class LIFOCache(BaseCaching):
-    """ BaseCaching defines:
-    Create a class FIFOCache that inherits from
-    BaseCaching and is a caching system:
-    """
+    """ LIFO Caching System """
 
     def __init__(self):
-        """ function init
-        """
+        """ Initialize the cache """
         super().__init__()
+        self.order = []  # List to keep track of the order of insertion
 
     def put(self, key, item):
-        """ function put
-        """
+        """ Add an item to the cache """
         if key and item:
             if key in self.cache_data:
-                self.cache_data.pop(key)
+                self.order.remove(key)  # Remove key from order if it already exists
+
             self.cache_data[key] = item
+            self.order.append(key)  # Add the key to the end of the order list
+
+            # If cache exceeds the maximum number of items, discard the last added item
             if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-                last_key = list(self.cache_data)[3]
+                last_key = self.order.pop(-2)  # Remove the second-last item (LIFO)
                 print(f"DISCARD: {last_key}")
                 del self.cache_data[last_key]
 
     def get(self, key):
-        """ function get
-        """
-        if key and key in self.cache_data:
-            return self.cache_data[key]
-        else:
-            return None
+        """ Retrieve an item from the cache """
+        return self.cache_data.get(key, None)
