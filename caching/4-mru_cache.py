12be74ba4bf7881191a700a18f75b3152b9039b6
melekmoalla <melekmoalla2468@gmail.com>
2024-09-19 20:13:03 +0100

    it's an amazing task

--- caching/4-mru_cache.py
+++ caching/4-mru_cache.py
@@ -8,7 +8,7 @@ BaseCaching and is a caching system:
 from base_caching import BaseCaching
 
 
-class LRUCache(BaseCaching):
+class MRUCache(BaseCaching):
     """ BaseCaching defines:
     Create a class FIFOCache that inherits from
     BaseCaching and is a caching system:
@@ -25,23 +25,29 @@ class LRUCache(BaseCaching):
         if key and item:
             if key in self.cache_data:
                 self.cache_data.pop(key)
+                items = list(self.cache_data.items())
+                items.insert(0, (key, item))
+                self.cache_data = dict(items)
 
-            self.cache_data[key] = item
-            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            elif len(self.cache_data) >= BaseCaching.MAX_ITEMS:
                 last_key = list(self.cache_data)[0]
+                self.cache_data.pop(last_key)
                 print(f"DISCARD: {last_key}")
-                del self.cache_data[last_key]
+
+                items = list(self.cache_data.items())
+                items.insert(0, (key, item))
+                self.cache_data = dict(items)
+            else:
+                self.cache_data[key] = item
 
     def get(self, key):
         """ function get
         """
-        print("aaa")
         if key and key in self.cache_data:
             if key in self.cache_data:
-                value = self.cache_data[key]
-                self.cache_data.pop(key)
-                self.cache_data[key] = value
-            print(value)
-            return value
+                items = list(self.cache_data.items())
+                items.insert(0, (key, self.cache_data[key]))
+                self.cache_data = dict(items)
+            return self.cache_data[key]
         else:
             return None
