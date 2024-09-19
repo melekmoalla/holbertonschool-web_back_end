502eb51ea5ba743e5d97368c02281657fa5f5e54
melekmoalla <melekmoalla2468@gmail.com>
2024-09-19 13:45:50 +0100

    fifo

---
+++ caching/2-lifo_cache.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+""" BaseCaching defines:
+Create a class FIFOCache that inherits from
+BaseCaching and is a caching system:
+"""
+
+from base_caching import BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """ BaseCaching defines:
+    Create a class FIFOCache that inherits from
+    BaseCaching and is a caching system:
+    """
+
+    def __init__(self):
+        """ function init
+        """
+        super().__init__()
+
+    def put(self, key, item):
+        """ function put
+        """
+        if key and item:
+            self.cache_data[key] = item
+            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+                name = list(self.cache_data)[3]
+                print('DISCARD:' + ' ' + name)
+                self.cache_data.pop(name)
+
+    def get(self, key):
+        """ function get
+        """
+        if key and key in self.cache_data:
+            return self.cache_data[key]
+        else:
+            return None
