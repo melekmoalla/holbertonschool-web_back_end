ba17719d0f74d6d0c7bcd2c76f06e1860d1717f5
melekmoalla <melekmoalla2468@gmail.com>
2024-10-07 23:50:00 +0100

    test

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 """Module to filter and obfuscate sensitive data in log messages."""
+import logging
 import re
 from typing import List
 
@@ -27,3 +28,27 @@ def filter_datum(
             f"{test}={redaction}",
             message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        NotImplementedError
+        original_message = record.getMessage()
+        print(original_message)
+        redacted_message = filter_datum(
+            self.fields,
+            self.REDACTION,
+            original_message,
+            self.SEPARATOR)
+        return redacted_message
