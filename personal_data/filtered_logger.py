94383391f0e259ae196ff31755e06ed7c69f8d0e
melekmoalla <melekmoalla2468@gmail.com>
2024-10-07 18:45:27 +0100

    test pep8

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -3,7 +3,12 @@
 import re
 from typing import List
 
-def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+
+def filter_datum(
+        fields: List[str],
+        redaction: str,
+        message: str,
+        separator: str) -> str:
     """
     Obfuscates the values of specified fields in a log message.
 
@@ -17,4 +22,7 @@ def filter_datum(fields: List[str], redaction: str, message: str, separator: str
         str: The obfuscated log message.
     """
     pattern = f"({'|'.join(fields)})=[^{separator}]+"
-    return re.sub(pattern, lambda match: f"{match.group(1)}={redaction}", message)
+    return re.sub(
+        pattern,
+        lambda match: f"{match.group(1)}={redaction}",
+        message)
