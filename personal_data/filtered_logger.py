5494fcaf7c2a91bf994c9108a95fd4feee5d563b
melekmoalla <melekmoalla2468@gmail.com>
2024-10-07 18:35:18 +0100

    first task

---
+++ personal_data/filtered_logger.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+import re
+
+def filter_datum(fields, redaction, message, separator):
+    for field in fields:
+        message = re.sub(f"{field}=[^{separator}]+", f"{field}={redaction}", message)
+    return message
+
+
+"""def filter_datum(fields, redaction, message, separator):
+    value = ""
+    ph = ""
+    list = message.split(separator)
+
+    for i in list[:-1]:
+        lis= i.split('=')
+        for b in fields:
+            if b == lis[0]:
+                value += lis[0]+'='+redaction          
+                break
+        if value != "":
+            ph += value + separator
+            value = ""
+        else:
+            ph += i + separator
+    return ph"""
