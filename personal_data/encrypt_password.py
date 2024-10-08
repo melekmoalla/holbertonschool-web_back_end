cc188b136f78651b578dd3b6746c2d455aa2f0e3
melekmoalla <melekmoalla2468@gmail.com>
2024-10-08 16:37:47 +0100

    tatsk passworld cryption

---
+++ personal_data/encrypt_password.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+hash_password function that expects one string argument
+ name password and returns a salted,
+ hashed password, which is a byte string.
+"""
+
+import bcrypt
+
+
+def hash_password(password):
+    bytes = password.encode('utf-8')
+    salt = bcrypt.gensalt()
+    hash = bcrypt.hashpw(bytes, salt)
+    return hash
