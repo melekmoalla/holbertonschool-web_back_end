6fd5b5e8e87a86fbb9f16e1466f416e3798daf50
melekmoalla <melekmoalla2468@gmail.com>
2024-10-08 17:18:07 +0100

    finally task

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -18,3 +18,14 @@ def hash_password(password: str) -> bytes:
     salt = bcrypt.gensalt()
     hash = bcrypt.hashpw(bytes, salt)
     return hash
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    Implement an is_valid function that expects 2
+    arguments and returns a boolean.
+
+    """
+    userBytes = password.encode('utf-8')
+    result = bcrypt.checkpw(userBytes, hashed_password)
+    return result
