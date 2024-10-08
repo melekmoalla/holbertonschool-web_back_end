0964a7cb4692d52e782675d4cc78fce15b54caef
melekmoalla <melekmoalla2468@gmail.com>
2024-10-08 16:38:45 +0100

    test problem

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -7,8 +7,12 @@ hash_password function that expects one string argument
 
 import bcrypt
 
-
 def hash_password(password):
+    """
+    hash_password function that expects one string argument
+    name password and returns a salted,
+    hashed password, which is a byte string.
+    """
     bytes = password.encode('utf-8')
     salt = bcrypt.gensalt()
     hash = bcrypt.hashpw(bytes, salt)
