0eb38c94feaa5615beb81b410cf6da68623241c7
melekmoalla <melekmoalla2468@gmail.com>
2024-10-09 16:45:49 +0100

    test

--- Basic_authentication/api/v1/views/index.py
+++ Basic_authentication/api/v1/views/index.py
@@ -25,7 +25,7 @@ def stats() -> str:
     stats['users'] = User.count()
     return jsonify(stats)
 
-@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
+@app_views.route('/api/v1/unauthorized', methods=['GET'], strict_slashes=False)
 def unauthorized() -> str:
     """Endpoint that raises a 401 error."""
     abort(401)
