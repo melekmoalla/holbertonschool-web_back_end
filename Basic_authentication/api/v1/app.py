3e5f60ae89bdd6aae95485d0ebfb8155994df64c
melekmoalla <melekmoalla2468@gmail.com>
2024-10-09 20:49:06 +0100

    it's the bts task 5

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -6,12 +6,17 @@ from os import getenv
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
+from api.v1.auth.auth import Auth
 import os
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+auth = None
+auth_type = getenv('AUTH_TYPE')
+if auth_type == 'auth':
+    auth = Auth()
 
 
 @app.errorhandler(404)
@@ -33,6 +38,23 @@ def forbidden(error) -> str:
     """ Not found handler
     """
     return jsonify({"error": "Forbidden"}), 403
+
+
+@app.before_request
+def before_request():
+    """ handler before_request
+    """
+    if auth is None:
+        return
+    list = ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']
+    if request.path not in list:
+        if not auth.require_auth(request.path, list):
+            return
+
+    if auth.authorization_header(request) is None:
+        abort(401)
+    if auth.current_user(request) is None:
+        abort(403)
 
 
 if __name__ == "__main__":
--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -34,7 +34,11 @@ class Auth():
     def authorization_header(self, request=None) -> str:
         """public method def authorization_header(self, request=None)
         -> str: that returns None - request will be the Flask request object"""
-        return None
+        if request is None:
+            return None
+        if 'Authorization' not in request.headers:
+            return None
+        return request.headers.get('Authorization')
 
     def current_user(self, request=None) -> TypeVar('User'):
         """public method def current_user(self, request=None)
