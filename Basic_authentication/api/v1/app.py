4fa9455f9ebef3a0d1d88b9c28772abe0eae4c9e
melekmoalla <melekmoalla2468@gmail.com>
2024-10-09 16:46:37 +0100

    another test for task 1

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -21,7 +21,7 @@ def not_found(error) -> str:
     return jsonify({"error": "Not found"}), 404
 
 @app.errorhandler(401)
-def page_not_found(error) -> str:
+def Unauthorized(error) -> str:
     """ Not found handler
     """
     return jsonify({"error": "Unauthorized"}), 401
