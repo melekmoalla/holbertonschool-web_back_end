343855e623058d501dc3bc3e1e96e4dd99f8a200
melekmoalla <melekmoalla2468@gmail.com>
2024-10-09 16:44:16 +0100

    add -> str: for fix the issue

--- Basic_authentication/api/v1/views/index.py
+++ Basic_authentication/api/v1/views/index.py
@@ -26,6 +26,6 @@ def stats() -> str:
     return jsonify(stats)
 
 @app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
-def unauthorized():
+def unauthorized() -> str:
     """Endpoint that raises a 401 error."""
     abort(401)
