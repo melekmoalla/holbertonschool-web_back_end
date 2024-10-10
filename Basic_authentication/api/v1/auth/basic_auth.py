0c76e2a807147e0a146452ad14455ce7f3eb82ea
melekmoalla <melekmoalla2468@gmail.com>
2024-10-11 00:57:41 +0100

    i <ant just test the porblme from where

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -55,25 +55,19 @@ class BasicAuth(Auth):
         m, m1 = decoded_base64_authorization_header.split(':')
 
         return m, m1
-
-    def user_object_from_credentials(self, user_email: str,
-                                     user_pwd: str) -> TypeVar('User'):
-        """
-        that returns the User instance based on his email and password.
-        """
+    def user_object_from_credentials(self, user_email: str, user_pwd: str
+                                     ) -> TypeVar('User'):
+        """ returns the User instance based on his email and password """
         if not user_email and not user_pwd:
-            return None
-        if not isinstance(user_email, str) and not isinstance(user_pwd, str):
-            return None
-
-        user = User.search({"email": user_email})
-        if not user:
-            return None
-        user = user[0]
-        if not user.is_valid_password(user_pwd):
-            return None
-
-        return user
+            if type(user_email) is not str and type(user_pwd) is not str:
+                return None
+        from models.user import User
+        users = User.search({"email": user_email})
+        if users:
+            for user in users:
+                if user.is_valid_password(user_pwd):
+                    return user
+        return None
 
     def current_user(self, request=None) -> TypeVar('User'):
         """
@@ -90,3 +84,4 @@ class BasicAuth(Auth):
         user_auth_header = self.user_object_from_credentials(email, pawd)
 
         return user_auth_header
+ 
