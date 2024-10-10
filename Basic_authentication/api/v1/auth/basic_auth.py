b67465fee0ac3978672eae9edff721bdecaedc11
melekmoalla <melekmoalla2468@gmail.com>
2024-10-10 09:41:08 +0100

    task 7

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -9,4 +9,17 @@ class BasicAuth(Auth):
     """
     BasicAuth that inherits from Auth
     """
-    pass
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """
+       class BasicAuth that returns the Base64
+       part of the Authorization header for a
+       Basic Authentication:
+        """
+        if authorization_header is None:
+            return None
+        if type(authorization_header) != str:
+            return None
+        if not authorization_header.startswith('Basic '):
+            return None
+        return authorization_header[6:]
