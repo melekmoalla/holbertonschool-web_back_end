bed1cfd947211afeb0d8ffc7f1caef7bb21053fe
melekmoalla <melekmoalla2468@gmail.com>
2024-10-10 10:13:00 +0100

    task 8 decode_base64_authorization_header

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,12 +3,14 @@
 Route module for the API
 """
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
     """
     BasicAuth that inherits from Auth
     """
+
     def extract_base64_authorization_header(self,
                                             authorization_header: str) -> str:
         """Extracts the Base64 part of the Authorization header."""
@@ -19,3 +21,20 @@ class BasicAuth(Auth):
         if not authorization_header.startswith('Basic '):
             return None
         return authorization_header[6:]
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header:
+                                           str) -> str:
+        """
+        that returns the decoded value of a
+        Base64 string base64_authorization_header:
+        """
+        if base64_authorization_header is None:
+            return None
+        if type(base64_authorization_header) is not str:
+            return None
+        try:
+            decoded_data = base64.b64decode(base64_authorization_header)
+            return decoded_data.decode('utf-8')
+        except Exception:
+            return None
