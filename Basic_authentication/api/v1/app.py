13780f0319803e1fd1b3c528684af568b32c6d4e
melekmoalla <melekmoalla2468@gmail.com>
2024-10-11 15:07:45 +0100

    test now

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -51,13 +51,13 @@ def before_request():
     """
     if auth is None:
         return
-    list = ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']
-    if request.path not in list:
-        if not auth.require_auth(request.path, list):
-            return
+    excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']
+    if not auth.require_auth(request.path, excluded_paths):
+        return
 
     if auth.authorization_header(request) is None:
         abort(401)
+
     request.current_user = auth.current_user(request)
 
     if request.current_user is None:
