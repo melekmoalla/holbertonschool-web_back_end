9afff90764ae9545495b43e44010aa2a992e6da7
melekmoalla <melekmoalla2468@gmail.com>
2024-10-13 00:39:50 +0100

    test

--- Session_authentication/api/v1/views/__init__.py
+++ Session_authentication/api/v1/views/__init__.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
 """ DocDocDocDocDocDoc
 """
-
-
 from flask import Blueprint
 
 
--- Session_authentication/api/v1/views/session_auth.py
+++ Session_authentication/api/v1/views/session_auth.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 """
 Create a new Flask view that handles all routes for the Session authentication.
-
 """
 from flask import jsonify, abort, request, Response
 from api.v1.views import app_views
@@ -15,25 +14,4 @@ def session_auth_login():
     """
     Handles the session login and returns a Flask Response object.
     """
-    email = request.form.get('email')
-    password = request.form.get('password')
-
-    if not email:
-        return ({"error": "email missing"}), 400
-    if not password:
-        return ({"error": "password missing"}), 400
-    users = User.search({'email': email})
-
-    if not users:
-        return jsonify({"error": "no user found for this email"}), 404
-    user = users[0]
-    if not user.is_valid_password(password):
-        return jsonify({"error": "wrong password"}), 401
-    session_id = auth.create_session(user.id)
-    user_json = user.to_json()
-    response = jsonify(user_json)
-
-    session_name = os.getenv("SESSION_NAME")
-    response.set_cookie(session_name, session_id)
-
-    return response
+    pass
