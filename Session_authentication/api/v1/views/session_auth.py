0df59f77ee2be6d589116b3edb0d6e508a70cd87
melekmoalla <melekmoalla2468@gmail.com>
2024-10-13 00:47:21 +0100

    pepe8

--- Session_authentication/api/v1/views/session_auth.py
+++ Session_authentication/api/v1/views/session_auth.py
@@ -27,7 +27,7 @@ def session_auth_login():
     user = users[0]
     if not user.is_valid_password(password):
         return jsonify({"error": "wrong password"}), 401
-    
+
     from api.v1.app import auth
 
     session_id = auth.create_session(user.id)
