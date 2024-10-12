7e93f3bce780fdfc415bb2f0748736738843f863
melekmoalla <melekmoalla2468@gmail.com>
2024-10-13 00:49:22 +0100

    test

--- Session_authentication/api/v1/views/__init__.py
+++ Session_authentication/api/v1/views/__init__.py
@@ -3,7 +3,6 @@
 """
 from api.v1.views.users import *
 from api.v1.views.index import *
-from api.v1.views.session_auth import *
 
 from flask import Blueprint
 
@@ -12,3 +11,4 @@ app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 
 
 User.load_from_file()
+from api.v1.views.session_auth import *
