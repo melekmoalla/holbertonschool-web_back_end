412af31febae9cce5675a97c658a576820646395
melekmoalla <melekmoalla2468@gmail.com>
2024-10-12 17:51:51 +0100

    corret pep8

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -60,11 +60,12 @@ def before_request():
         '/api/v1/unauthorized/',
         '/api/v1/forbidden/',
         "/api/v1/auth_session/login/"]
-    
+
     if not auth.require_auth(request.path, list):
         return
 
-    if auth.authorization_header(request) is None and auth.session_cookie(request) is None:
+    if auth.authorization_header(
+            request) is None and auth.session_cookie(request) is None:
         abort(401)
     if auth.current_user(request) is None:
         abort(403)
