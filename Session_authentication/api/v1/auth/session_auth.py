61e188cccf30f5bda5dbdedf89cdba05b056960c
melekmoalla <melekmoalla2468@gmail.com>
2024-10-12 17:13:20 +0100

    task 3a like that methode of sessions

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -24,3 +24,12 @@ class SessionAuth(Auth):
         Session = str(uuid.uuid4())
         self.user_id_by_session_id[Session] = user_id
         return Session
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """
+        get any session
+        """
+        if session_id is None or not isinstance(session_id, str):
+            return None
+
+        return self.user_id_by_session_id.get(session_id)
