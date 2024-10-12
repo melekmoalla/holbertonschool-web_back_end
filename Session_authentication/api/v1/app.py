f81b5923f6240beac9bd5d7c42ec2d6b3ad5ddda
melekmoalla <melekmoalla2468@gmail.com>
2024-10-12 17:50:59 +0100

    correct task 5

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -60,13 +60,11 @@ def before_request():
         '/api/v1/unauthorized/',
         '/api/v1/forbidden/',
         "/api/v1/auth_session/login/"]
-    if request.path not in list:
-        if not auth.require_auth(request.path, list):
-            return
-    if auth.authorization_header(request) and auth.session_cookie(request):
-        return None, abort(401)
+    
+    if not auth.require_auth(request.path, list):
+        return
 
-    if auth.authorization_header(request) is None:
+    if auth.authorization_header(request) is None and auth.session_cookie(request) is None:
         abort(401)
     if auth.current_user(request) is None:
         abort(403)
