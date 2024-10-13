175040f5237e42993da55e0192672ca8936c05be
melekmoalla <melekmoalla2468@gmail.com>
2024-10-13 01:21:07 +0100

    task

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -53,6 +53,10 @@ class SessionAuth(Auth):
         if not session_id:
             return False
 
+        user_id = self.user_id_for_session_id(session_id)
+        if user_id is None:
+            return False
+
         del self.user_id_by_session_id[session_id]
 
         return True
