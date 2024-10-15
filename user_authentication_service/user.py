07b701c7f6d21618142591957c331411c4cd4342
melekmoalla <melekmoalla2468@gmail.com>
2024-10-15 18:21:03 +0100

    test

--- user_authentication_service/user.py
+++ user_authentication_service/user.py
@@ -21,3 +21,6 @@ class User(Base):
     hashed_password = Column(VARCHAR(250), nullable=False)
     session_id = Column(VARCHAR(250), nullable=True)
     reset_token = Column(VARCHAR(250), nullable=True)
+
+    def __repr__(self):
+        return "<User(id='{}', email='{}')>".format(self.id, self.email)
