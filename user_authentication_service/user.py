42356a0526886c11e9a46780a97d55a220a1d6c6
melekmoalla <melekmoalla2468@gmail.com>
2024-10-15 18:19:20 +0100

    tst

--- user_authentication_service/user.py
+++ user_authentication_service/user.py
@@ -21,7 +21,3 @@ class User(Base):
     hashed_password = Column(VARCHAR(250), nullable=False)
     session_id = Column(VARCHAR(250), nullable=True)
     reset_token = Column(VARCHAR(250), nullable=True)
-
-    def __repr__(self):
-        return "<User(id='%s', email='%s')>" % (
-            self.id, self.email)
