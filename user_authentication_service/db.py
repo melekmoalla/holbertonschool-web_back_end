d0364a1b61f742acd1148a308256a9e16c7479b2
melekmoalla <melekmoalla2468@gmail.com>
2024-10-16 11:44:57 +0100

    test

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -65,7 +65,5 @@ class DB:
             for key, value in kwargs.items():
                 setattr(user, key, value)
             self._session.commit()
-        except NoResultFound:
-            raise NoResultFound
-        except InvalidRequestError:
-            raise InvalidRequestError
+        except Exception:
+            raise ValueError()
