583f31a52b0146530fdda023977c53479807f3ea
melekmoalla <melekmoalla2468@gmail.com>
2024-10-16 12:12:18 +0100

    task 5

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -3,6 +3,9 @@
 Main file
 """
 import bcrypt
+from db import DB
+from sqlalchemy.orm.exc import NoResultFound
+from user import User
 
 
 def _hash_password(password: str) -> bytes:
@@ -15,3 +18,23 @@ def _hash_password(password: str) -> bytes:
     salt = bcrypt.gensalt()
     hashed = bcrypt.hashpw(password.encode('utf-8'), salt)
     return hashed
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """Auth.register_user should take mandatory email and
+        password string arguments and return a User object
+        """
+        try:
+            self._db.find_user_by(email=email)
+            raise ValueError(f"User {email} already exists")
+        except NoResultFound:
+            hashed_password = _hash_password(password)
+            user = self._db.add_user(email, hashed_password)
+            return user
