09a5b1e1dc477d4cbf1277b2165586bb4b2a4832
melekmoalla <melekmoalla2468@gmail.com>
2024-10-17 10:46:54 +0100

    test

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -6,6 +6,7 @@ import bcrypt
 from db import DB
 from sqlalchemy.orm.exc import NoResultFound
 from user import User
+import uuid
 
 
 def _hash_password(password: str) -> bytes:
@@ -54,8 +55,6 @@ class Auth:
             return False
 
     def _generate_uuid(self) -> str:
-        import uuid
-
         """
         The function should return a string
         representation of a new UUID. Use the
