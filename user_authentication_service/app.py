db1a48f030b41f3e289682f73c964d23e075b942
melekmoalla <melekmoalla2468@gmail.com>
2024-10-17 09:59:30 +0100

    task 7 something amazing

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -2,9 +2,14 @@
 """
 Flask application to return a welcome message.
 """
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
+from sqlalchemy.orm.exc import NoResultFound
+import os
 
 app = Flask(__name__)
+
+AUTH = Auth()
 
 
 @app.route("/", methods=["GET"])
@@ -13,5 +18,23 @@ def welcome():
     return jsonify({"message": "Bienvenue"})
 
 
+@app.route('/users', methods=['POST'])
+def users():
+    """
+    Define a users function that implements the
+    POST /users route.
+    Import the Auth object and instantiate it
+    at the root of the module
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    try:
+        user = AUTH.register_user(email, password)
+        return jsonify({"email": user.email, "message": "user created"}), 200
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
+
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000")
+    app.run(host="0.0.0.0", port="4000")
