f5e2a37524c3681b7a26089fa56ac1f3c553d994
melekmoalla <melekmoalla2468@gmail.com>
2024-10-17 11:15:28 +0100

    task 11

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -2,7 +2,7 @@
 """
 Flask application to return a welcome message.
 """
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort
 from auth import Auth
 from sqlalchemy.orm.exc import NoResultFound
 import os
@@ -34,6 +34,27 @@ def users():
         return jsonify({"email": user.email, "message": "user created"}), 200
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route('/sessions', methods=['POST'])
+def login():
+    """
+    The request is expected to contain form data
+    with "email" and a "password" fields.
+    If the login information is incorrect, use
+    flask.abort to respond with a 401 HTTP status.
+    Otherwise, create a new session for the user,
+    store it the session ID as a cookie with key "session_id"
+    on the response and return a JSON payload of the form"""
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    if not AUTH.valid_login(email, password):
+        abort(401)
+    session_id = AUTH.create_session(email)
+    response = jsonify({"email": email, "message": "logged in"})
+    response.set_cookie('session_id', session_id)
+    return response
 
 
 if __name__ == "__main__":
