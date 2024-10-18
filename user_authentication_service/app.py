0f50e3327b0284396ccce7ed91783275cfa81fa1
melekmoalla <melekmoalla2468@gmail.com>
2024-10-18 11:26:55 +0100

    task 15

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -86,6 +86,8 @@ def profile():
     If the user exist, respond with a 200 HTTP
     """
     session_id = request.cookies.get('session_id')
+    if not session_id:
+        abort(403)
     user = AUTH.get_user_from_session_id(session_id)
     if user:
         return jsonify({"email": user["email"]}), 200
