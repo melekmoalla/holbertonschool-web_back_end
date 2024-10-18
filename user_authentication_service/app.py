eb3218d28350ade85f9b44b7ad3484007469c7ee
melekmoalla <melekmoalla2468@gmail.com>
2024-10-18 11:28:55 +0100

    test

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -90,7 +90,7 @@ def profile():
         abort(403)
     user = AUTH.get_user_from_session_id(session_id)
     if user:
-        return jsonify({"email": user["email"]}), 200
+        return jsonify({"email": user.email}), 200
     else:
         abort(403)
 
