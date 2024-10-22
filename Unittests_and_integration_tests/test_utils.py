8440f9d593baeac98de455978585814b530bcfd0
melekmoalla <melekmoalla2468@gmail.com>
2024-10-22 16:39:53 +0100

    ad line  mock_get.assert_called_once_with(test_url)

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -57,6 +57,6 @@ class TestGetJson(unittest.TestCase):
             mock_response.json.return_value = test_payload
             mock_get.return_value = mock_response
             result = get_json(test_url)
-
+            mock_get.assert_called_once_with(test_url)
             self.assertEqual(result, test_payload)
             mock_get.reset_mock()
