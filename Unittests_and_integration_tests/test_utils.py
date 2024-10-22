3d866297f331169a65218f49b286d33a36f6be22
melekmoalla <melekmoalla2468@gmail.com>
2024-10-22 18:38:15 +0100

    task

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -81,6 +81,15 @@ class TestMemoize(unittest.TestCase):
           is returned but a_method is only called once using
            assert_called_once.
         """
+        class TestClass:
+
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+
         test_obj = self.TestClass()
         self.assertEqual(test_obj.a_property, 42)
         self.assertEqual(test_obj.a_property, 42)
