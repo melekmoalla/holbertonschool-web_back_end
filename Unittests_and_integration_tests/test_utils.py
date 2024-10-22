1a235733ef3b1282becb3e6f9cb5ce832c5eb8e6
melekmoalla <melekmoalla2468@gmail.com>
2024-10-22 19:25:15 +0100

    test

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -85,9 +85,13 @@ class TestMemoize(unittest.TestCase):
             def a_property(self):
                 return self.a_method()
 
-        with patch.object(TestClass, 'a_method') as mock_method:
+        with patch.object(TestClass, 'a_method', return_value=42) as mock_method:
 
             test_obj = TestClass()
-            test_obj.a_property()
-            test_obj.a_property()
+            self.assertEqual(test_obj.a_property, 42)
+            self.assertEqual(test_obj.a_property, 42)
             mock_method.assert_called_once()
+
+
+if __name__ == '__main__':
+    unittest.main()
