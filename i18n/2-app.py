ff9494327d848c466805bded74e5aa208ff0e6b0
melekmoalla <melekmoalla2468@gmail.com>
2024-10-23 10:42:31 +0100

    test

--- i18n/2-app.py
+++ i18n/2-app.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
-from flask import Flask, render_template
+"""
+learn flask_babel
+"""
+from flask import Flask, render_template, request
 from flask_babel import Babel
 
 
@@ -16,12 +19,17 @@ app.config.from_object(Config)
 babel = Babel(app)
 
 
+@babel.localeselector
+def get_locale():
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
 @app.route('/')
 def index():
     """
     a single / route and an index.html template that simply
     """
-    return render_template('1-index.html')
+    return render_template('2-index.html')
 
 
 if __name__ == '__main__':
