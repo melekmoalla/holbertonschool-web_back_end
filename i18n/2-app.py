c9d41eeb8d546508aaa95aa2a7846813e8833dd7
melekmoalla <melekmoalla2468@gmail.com>
2024-10-23 10:57:04 +0100

    test

--- i18n/2-app.py
+++ i18n/2-app.py
@@ -27,6 +27,9 @@ babel = Babel(app)
 
 @babel.localeselector
 def get_locale():
+    """
+    determine the best match with our supported languages.
+    """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
