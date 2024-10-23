4a172a50af498a6009c905a20f00bb193f6f3aab
melekmoalla <melekmoalla2468@gmail.com>
2024-10-23 09:43:11 +0100

    task 0 and 1

--- i18n/0-app.py
+++ i18n/0-app.py
@@ -4,7 +4,6 @@ from flask_babel import Babel
 
 
 app = Flask(__name__)
-babel = Babel(app)
 
 
 @app.route('/')
--- i18n/1-app.py
+++ i18n/1-app.py
@@ -4,6 +4,12 @@ from flask_babel import Babel
 
 
 app = Flask(__name__)
+class Config:
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+app.config.from_object(Config)
 babel = Babel(app)
 
 @app.route('/')
