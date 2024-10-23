fe28244877bf2da246c707cd4f0129507a087b94
melekmoalla <melekmoalla2468@gmail.com>
2024-10-23 16:35:34 +0100

    test

--- i18n/3-app.py
+++ i18n/3-app.py
@@ -55,10 +55,7 @@ def index() -> str:
     """
     return render_template('3-index.html')
 
-@babel.localeselector
-def get_locale():
-    """ to determine the best match with our supported languages """
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
 
 if __name__ == '__main__':
     app.run()
