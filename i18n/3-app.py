12abaade345221bd4d82c178d606632369ae0929
melekmoalla <melekmoalla2468@gmail.com>
2024-10-23 11:44:02 +0100

    task 3 it's amazing something new for me

---
+++ i18n/3-app.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+"""
+learn flask_babel
+"""
+from flask import Flask, render_template, request
+from flask_babel import Babel, _
+
+app = Flask(__name__)
+
+
+class Config:
+    """
+    In order to configure available languages
+    in our app, you will create a Config class
+    that has a LANGUAGES class attribute equal
+    to ["en", "fr"]
+    """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'fr'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+
+
+def get_locale():
+    """
+    Determine the best match with our supported languages.
+    """
+    lang = request.args.get('lang')
+    if lang in app.config['LANGUAGES']:
+        return lang
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+babel = Babel(app, locale_selector=get_locale)
+
+
+@app.route('/')
+def index():
+    """
+    A single / route and an index.html template that simply
+    displays the translated strings.
+    """
+    return render_template('3-index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
