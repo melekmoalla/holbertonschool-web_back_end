3827ae9b02f7362aea42761f4ac7430361d14e1e
melekmoalla <melekmoalla2468@gmail.com>
2024-10-23 17:50:52 +0100

    test

--- i18n/5-app.py
+++ i18n/5-app.py
@@ -5,7 +5,7 @@ learn flask_babel
 from flask import Flask, render_template, request, g
 from flask_babel import Babel, _
 
-app = Flask(__name__, template_folder="templates")
+app = Flask(__name__)
 
 users = {
     1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
