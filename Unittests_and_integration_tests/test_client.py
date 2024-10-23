d12bfeb8b163e1e5488520dd04be5be2f5a17890
melekmoalla <melekmoalla2468@gmail.com>
2024-10-23 09:27:58 +0100

    task 0

---
+++ Unittests_and_integration_tests/test_client.py
@@ -0,0 +1,11 @@
+
+
+
+class TestGithubOrgClient(unittest.TestCase):
+
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    @patch('utils.requests.get')
+    def test_org():
