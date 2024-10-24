38ec4136dcd223cf3416fa7ca75e01391117808e
melekmoalla <melekmoalla2468@gmail.com>
2024-10-24 16:14:14 +0100

    test

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -73,11 +73,6 @@ class TestMemoize(unittest.TestCase):
            assert_called_once.
         """
         class TestClass:
-            """
-            Read about memoization and familiarize
-            yourself with the utils.memoize decorator.
-            """
-
             def a_method(self):
                 return 42
 
@@ -89,6 +84,6 @@ class TestMemoize(unittest.TestCase):
                           'a_method', return_value=42) as mock_method:
 
             test_obj = TestClass()
-            self.assertEqual(test_obj.a_property, 42)
-            self.assertEqual(test_obj.a_property, 42)
+            self.assertEqual(test_obj.a_property(), 42)
+            self.assertEqual(test_obj.a_property(), 42)
             mock_method.assert_called_once()
