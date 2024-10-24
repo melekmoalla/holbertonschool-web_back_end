b4b0646d0e58b01c9ea713ec7a5aafb1d763075e
melekmoalla <melekmoalla2468@gmail.com>
2024-10-24 11:32:17 +0100

    test_public_repos_url I start now to figure out it

--- Unittests_and_integration_tests/test_client.py
+++ Unittests_and_integration_tests/test_client.py
@@ -27,8 +27,25 @@ class TestGithubOrgClient(unittest.TestCase):
 
         client = GithubOrgClient(org_name)
         result = client.org
-        result = client.org
         test_url = f'https://api.github.com/orgs/{org_name}'
         mock_get_json.assert_called_once_with(test_url)
-        print(mock_get_json)
         self.assertEqual(result, expected)
+
+    @patch('client.GithubOrgClient.org')
+    def test_public_repos_url(self, mock_org):
+        """
+        test_public_repos_url method to unit-test
+        GithubOrgClient._public_repos_url.
+        Use patch as a context manager to patch
+        GithubOrgClient.org and make it return a known payload.
+        Test that the result of _public_repos_url
+        is the expected one based on the mocked payload.
+        """
+        test_url = f'https://api.github.com/orgs/test/repo'
+
+        mock_org.return_value = {'repos_url': test_url}
+
+        client = GithubOrgClient('test')
+        result = client._public_repos_url()
+
+        self.assertEqual(result, test_url)
