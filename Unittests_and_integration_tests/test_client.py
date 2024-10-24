12dc1f3597a457e96854125c2b321570df7cebef
melekmoalla <melekmoalla2468@gmail.com>
2024-10-24 10:30:57 +0100

    test

--- Unittests_and_integration_tests/test_client.py
+++ Unittests_and_integration_tests/test_client.py
@@ -27,6 +27,7 @@ class TestGithubOrgClient(unittest.TestCase):
 
         client = GithubOrgClient(org_name)
         result = client.org
+        result = client.org
         test_url = f'https://api.github.com/orgs/{org_name}'
         mock_get_json.assert_called_once_with(test_url)
         print(mock_get_json)
