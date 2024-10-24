5b765b050baf455ba4ce2f74ae73b85a7a05ab1a
melekmoalla <melekmoalla2468@gmail.com>
2024-10-24 16:16:21 +0100

    test to se the problem

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -60,18 +60,9 @@ class TestGetJson(unittest.TestCase):
 
 
 class TestMemoize(unittest.TestCase):
-    """
-    Read about memoization and familiarize
-    yourself with the utils.memoize decorator.
-    """
-
+    """Tests the `memoize` function."""
     def test_memoize(self) -> None:
-        """
-        Use unittest.mock.patch to mock a_method. Test that
-         when calling a_property twice, the correct result
-          is returned but a_method is only called once using
-           assert_called_once.
-        """
+        """Tests `memoize`'s output."""
         class TestClass:
             def a_method(self):
                 return 42
@@ -79,8 +70,12 @@ class TestMemoize(unittest.TestCase):
             @memoize
             def a_property(self):
                 return self.a_method()
-
-        with patch.object(TestClass,
-                          'a_method', return_value=42) as mock_method:
-
-            pass
+        with patch.object(
+                TestClass,
+                "a_method",
+                return_value=lambda: 42,
+                ) as memo_fxn:
+            test_class = TestClass()
+            self.assertEqual(test_class.a_property(), 42)
+            self.assertEqual(test_class.a_property(), 42)
+            memo_fxn.assert_called_once()
