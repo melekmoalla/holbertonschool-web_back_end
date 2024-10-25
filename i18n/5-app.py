c1201958c3d0a583c48778e76a579c6665e9c1dc
melekmoalla <melekmoalla2468@gmail.com>
2024-10-25 16:41:33 +0100

    task 5

--- i18n/5-app.py
+++ i18n/5-app.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python3
 """
-In order to configure available languages
-in our app, you will create a Config class
-that has a LANGUAGES class attribute equal
-to  ["en", "fr"]
+learn flask_babel
 """
 from flask import Flask, render_template, request, g
 from flask_babel import Babel
 
+app = Flask(__name__)
 
 users = {
     1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
@@ -16,60 +14,56 @@ users = {
     4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
 }
 
-
 class Config:
     """
     In order to configure available languages
     in our app, you will create a Config class
     that has a LANGUAGES class attribute equal
-    to  ["en", "fr"]
+    to ["en", "fr"]
     """
-    LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = "en"
-    BABEL_DEFAULT_TIMEZONE = "UTC"
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'fr'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
 
 
 def get_locale():
     """
     Determine the best match with our supported languages.
     """
-    locale = request.args.get('locale')
-    if locale in app.config['LANGUAGES']:
-        return locale
-    lang = request.args.get('lang')
+    lang = request.args.get('locale')
     if lang in app.config['LANGUAGES']:
         return lang
-
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
-app = Flask(__name__)
-app.config.from_object(Config)
 babel = Babel(app, locale_selector=get_locale)
 
-
 def get_user():
-    """Define a get_user function that returns a user
-    dictionary or None if the ID cannot be found or
-    if login_as was not passed.
     """
-    user_id = request.args.get('login_as')
-    if user_id and user_id.isdigit():
-        user_id = int(user_id)
-        return users.get(user_id)
-    return None
-
+    Define a get_user function that
+    returns a user dictionary or None
+    if the ID cannot be found or if
+    login_as was not passed.
+    """
+    user = request.args.get('login_as')
+    if user and user.isdigit():
+        user = int(user)
+        return users[user]
+    else:
+        return None
 
 @app.before_request
 def before_request():
     """
-    before_request should use
-      get_user to find a user
-      if any, and set it as a
-      global on flask.g.user.
+    Define a before_request function and
+    use the app.before_request decorator
+    to make it be executed before all
+    other functions.
     """
     g.user = get_user()
-
 
 @app.route('/')
 def index():
