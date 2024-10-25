5da48aa450a91eaec29b1fce1d5bfa1661b8d9a1
melekmoalla <melekmoalla2468@gmail.com>
2024-10-25 15:50:27 +0100

    remoev space from get user

--- i18n/5-app.py
+++ i18n/5-app.py
@@ -51,7 +51,6 @@ def get_user():
     dictionary or None if the ID cannot be found or
     if login_as was not passed.
     """
-
     user_id = request.args.get('login_as')
     if user_id and user_id.isdigit():
         user_id = int(user_id)
