90b4a4f91fa130416d3aa4a400927d789b28403e
melekmoalla <melekmoalla2468@gmail.com>
2024-10-25 21:04:50 +0100

    return

--- Unittests_and_integration_tests/test_client.py
+++ Unittests_and_integration_tests/test_client.py
@@ -94,4 +94,14 @@ class TestIntegrationGithubOrgClient(unittest.TestCase):
     class and implement the setUpClass and tearDownClass which
     are part of the unittest.TestCase API.
     """
-    pass
+    @classmethod
+    def setUpClass(cls):
+        """Start patching requests.get with fixtures."""
+        cls.get_patcher = patch('requests.get')
+        cls.mock_get = cls.get_patcher.start()
+        
+
+    @classmethod
+    def tearDownClass(cls):
+        """stop patching requests.get with fixtures."""
+        cls.get_patcher.stop()
