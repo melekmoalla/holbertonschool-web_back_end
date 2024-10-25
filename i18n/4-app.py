09e38b63fc5d37f39300fa21fd4a5538372c5ef5
melekmoalla <melekmoalla2468@gmail.com>
2024-10-25 15:49:54 +0100

    task 4 test documentation

--- i18n/4-app.py
+++ i18n/4-app.py
@@ -7,6 +7,12 @@ from flask_babel import Babel
 
 app = Flask(__name__)
 
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
 
 class Config:
     """
