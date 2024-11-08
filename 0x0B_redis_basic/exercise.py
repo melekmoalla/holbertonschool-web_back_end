16abd65c96f39b50e1e7491689a2f37c5e297fbb
melekmoalla <melekmoalla2468@gmail.com>
2024-11-08 12:55:02 +0100

    task 0

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -1,16 +1,26 @@
 #!/usr/bin/env python3
-
+"""
+0x0B_redis_basic file
+"""
 import redis
 import uuid
 
 
 class Cache:
+    """
+    0x0B_redis_basic file
+    """
     def __init__(self):
+        """
+        0x0B_redis_basic file
+        """
         self._redis = redis.Redis()
         self._redis.flushdb()
 
     def store(self, data) -> str:
-
+        """
+        0x0B_redis_basic file
+        """
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
