7878cddbd84a6cb7bcd6949ad4596b558f1229cd
melekmoalla <melekmoalla2468@gmail.com>
2024-11-08 12:45:29 +0100

    task 9

---
+++ 0x0B_redis_basic/exercise.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+0x0B_redis_basic file
+"""
+import redis
+import uuid
+
+class Cache:
+    def __init__(self):
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+    def store(self, data):
+
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
