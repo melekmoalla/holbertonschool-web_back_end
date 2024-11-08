77dc70f24cd4d3ac34e78c3ec45bb27155082593
melekmoalla <melekmoalla2468@gmail.com>
2024-11-08 12:58:16 +0100

    test now correct

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
+"""
+0x0B_redis_basic file
+"""
 import redis
 import uuid
 from typing import Union
@@ -6,20 +9,11 @@ from typing import Union
 
 class Cache:
     def __init__(self):
-        """Initialize the Redis client and flush the database."""
         self._redis = redis.Redis()
         self._redis.flushdb()
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
-        """
-        Store data in Redis with a randomly generated key.
 
-        Args:
-            data: The data to be stored, can be str, bytes, int, or float.
-
-        Returns:
-            The randomly generated key as a string.
-        """
-        key = str(uuid.uuid4())  # Generate a random UUID key
-        self._redis.set(key, data)  # Store data in Redis with the key
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
         return key
