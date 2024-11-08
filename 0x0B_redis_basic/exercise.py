8e2b6a88be628b4511d66948ea6f2713513bccd0
melekmoalla <melekmoalla2468@gmail.com>
2024-11-08 12:53:06 +0100

    test pep8

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -5,11 +5,13 @@
 import redis
 import uuid
 
+
 class Cache:
     def __init__(self):
         self._redis = redis.Redis()
         self._redis.flushdb()
-    def store(self, data):
+
+    def store(self, data) -> str:
 
         key = str(uuid.uuid4())
         self._redis.set(key, data)
