a5626798d0ab5b3d1bb116fa204721d9d9227955
melekmoalla <melekmoalla2468@gmail.com>
2024-11-10 19:56:01 +0100

    test

--- MySQL_Advanced/2-fans.sql
+++ MySQL_Advanced/2-fans.sql
@@ -1,6 +1,5 @@
---Import this table dump: metal_bands.sql.zip
---Column names must be: origin and nb_fans
---Your script can be executed on any database
+-- Import this table dump: metal_bands.sql.zip
+-- Column names must be: origin and nb_fans
 SELECT origin, SUM(fans) AS nb_fans
 FROM metal_bands
 GROUP BY origin
