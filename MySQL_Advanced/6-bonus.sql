41ab04a7cd9bf6e7625a27759720970a413a38f2
melekmoalla <melekmoalla2468@gmail.com>
2024-11-10 21:24:55 +0100

    test 4

--- MySQL_Advanced/6-bonus.sql
+++ MySQL_Advanced/6-bonus.sql
@@ -1,17 +1,28 @@
 -- Write a SQL script that creates a trigger that resets the attribute valid_email only when the email has been changed.
 DELIMITER $$
 
-CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
+CREATE PROCEDURE AddBonus(
+    IN user_id INT, 
+    IN project_name VARCHAR(255), 
+    IN score INT
+)
 BEGIN
-    DECLARE avg_score FLOAT;
-
-    SELECT AVG(score) INTO avg_score
-    FROM corrections
-    WHERE user_id = user_id;
-
-    UPDATE users
-    SET average_score = avg_score
-    WHERE id = user_id;
+    DECLARE project_id INT;
+    
+    SELECT id INTO project_id
+    FROM projects
+    WHERE name = project_name
+    LIMIT 1;
+    
+    IF project_id IS NULL THEN
+        INSERT INTO projects (name) VALUES (project_name);
+        SET project_id = LAST_INSERT_ID();
+    END IF;
+    
+    INSERT INTO corrections (user_id, project_id, score) 
+    VALUES (user_id, project_id, score);
+    
 END$$
 
 DELIMITER ;
+
