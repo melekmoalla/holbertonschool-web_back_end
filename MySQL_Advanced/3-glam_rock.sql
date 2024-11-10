3f31b355630092a6f4a36c4a81861a11cb61d3a5
melekmoalla <melekmoalla2468@gmail.com>
2024-11-10 22:30:20 +0100

    task 3

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -1,13 +1,7 @@
--- Write a SQL script that lists all bands with Glam
--- rock as their main style, ranked by their longevity
--- Calculate lifespan for Glam rock bands
-SELECT
-    band_name,
-    CASE
-        WHEN split IS NOT NULL THEN split - formed
-        ELSE YEAR(CURDATE()) - formed
-    END AS lifespan
+-- a SQL script that lists all bands with Glam rock as their main style, ranked by their longevity
+SELECT band_name,
+       YEAR(CURDATE()) - formed AS lifespan
 FROM metal_bands
-WHERE
-    main_style = 'Glam rock'
+WHERE main_style = 'Glam rock'
 ORDER BY lifespan DESC;
+
