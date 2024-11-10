3371d09fb8f73ed296f1a84a559bd16814ce4ff8
melekmoalla <melekmoalla2468@gmail.com>
2024-11-10 22:15:05 +0100

    test 8

--- MySQL_Advanced/8-index_my_names.sql
+++ MySQL_Advanced/8-index_my_names.sql
@@ -1,2 +1,3 @@
--- Create the index idx_name_first on the first letter of the 'name' column
+-- Create the index idx_name_first on 
+--the first letter of the 'name' column
 CREATE INDEX idx_name_first ON names (LEFT(name, 1));
