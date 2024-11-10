69efd0a58c095609068de3cf9d64be0a7e93e2b4
melekmoalla <melekmoalla2468@gmail.com>
2024-11-10 22:25:55 +0100

    test task 9

--- MySQL_Advanced/9-index_name_score.sql
+++ MySQL_Advanced/9-index_name_score.sql
@@ -1,5 +1,2 @@
--- With these attributes:
--- id, integer, never null, auto increment and primary key
--- email, string (255 characters), never null and unique
--- name, string (255 characters)
+-- a SQL script that creates an index idx_name_first_score on the table names and the first letter of name and the score.
 CREATE INDEX idx_name_first_score ON names ((name, 1), score);
