537ba8a1be6a1b611ac055972b0e32872fb96472
melekmoalla <melekmoalla2468@gmail.com>
2024-11-16 23:41:20 +0100

    test

--- unittests_in_js/0-calcul.test.js
+++ unittests_in_js/0-calcul.test.js
@@ -8,6 +8,10 @@ describe('calculateNumber', () => {
     assert.equal(calculateNumber(1, 3.7), 5);
     assert.equal(calculateNumber(1.2, 3.7), 5);
     assert.equal(calculateNumber(1.5, 3.7), 6);
+    assert.equal(calculateNumber(1, 3), 4);
+    assert.equal(calculateNumber(1, 3.7), 5);
+    assert.equal(calculateNumber(1.2, 3.7), 5);
+    assert.equal(calculateNumber(1.5, 3.7), 6);
 
     
 });
