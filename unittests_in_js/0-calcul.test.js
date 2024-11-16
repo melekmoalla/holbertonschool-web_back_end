5c121157b2aa0480b9e92a905fc2a656fd05800d
melekmoalla <melekmoalla2468@gmail.com>
2024-11-16 22:50:41 +0100

    test

--- unittests_in_js/0-calcul.test.js
+++ unittests_in_js/0-calcul.test.js
@@ -5,5 +5,9 @@ const calculateNumber = require('./0-calcul');
 describe('calculateNumber', () => {
    
     assert.equal(calculateNumber(1, 3), 4);
+    assert.equal(calculateNumber(1, 3.7), 5);
+    assert.equal(calculateNumber(1.2, 3.7), 5);
+    assert.equal(calculateNumber(1.5, 3.7), 6);
+
     
 });
