d5b81d93cdef4da9ad59c221a13a6837694f3bb5
melekmoalla <melekmoalla2468@gmail.com>
2024-11-16 23:31:30 +0100

    test

--- unittests_in_js/0-calcul.test.js
+++ unittests_in_js/0-calcul.test.js
@@ -1,13 +1,29 @@
 // 0-calcul.test.js
 const assert = require('assert');
-const calculateNumber = require('./0-calcul');
+const calculateNumber = require('./0-calcul.js');
 
-describe('calculateNumber', () => {
-   
-    assert.equal(calculateNumber(1, 3), 4);
-    assert.equal(calculateNumber(1, 3.7), 5);
-    assert.equal(calculateNumber(1.2, 3.7), 5);
-    assert.equal(calculateNumber(1.5, 3.7), 6);
+describe('calculateNumber', function() {
+  it('should return 4 for (1, 3)', function() {
+    assert.strictEqual(calculateNumber(1, 3), 4);
+  });
 
-    
+  it('should return 5 for (1, 3.7)', function() {
+    assert.strictEqual(calculateNumber(1, 3.7), 5);
+  });
+
+  it('should return 5 for (1.2, 3.7)', function() {
+    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+  });
+
+  it('should return 6 for (1.5, 3.7)', function() {
+    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+  });
+
+  it('should return 0 for (0, 0)', function() {
+    assert.strictEqual(calculateNumber(0, 0), 0);
+  });
+
+  it('should return 2 for (-1, 3)', function() {
+    assert.strictEqual(calculateNumber(-1, 3), 2);
+  });
 });
