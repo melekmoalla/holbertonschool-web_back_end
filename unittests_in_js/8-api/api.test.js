858e24a9bf27fa256737b20ede8e138b3c7b1e31
melekmoalla <melekmoalla2468@gmail.com>
2024-11-17 16:04:36 +0100

    test to found whre is the problme

--- unittests_in_js/8-api/api.test.js
+++ unittests_in_js/8-api/api.test.js
@@ -1,25 +1,13 @@
 const request = require('request');
 const { expect } = require('chai');
 
-describe('Index page', () => {
-  const baseUrl = 'http://localhost:7865';
+describe('API integration test', () => {
+  const API_URL = 'http://localhost:7865';
 
-  before((done) => {
-    require('./api');
-    done();
-  });
-
-  it('should return the correct status code for GET /', (done) => {
-    request.get(`${baseUrl}/`, (err, res, body) => {
-      expect(res.statusCode).to.equal(200);
-      done();
-    });
-  });
-
-  it('should return the correct result for GET /', (done) => {
-    request.get(`${baseUrl}/`, (err, res, body) => {
-    expect(res.statusCode).to.equal(200);
-      expect(body).to.equal('Welcome to the payment system');
+  it('GET / returns correct response', (done) => {
+    request.get(`${API_URL}/`, (_err, res, body) => {
+      expect(res.statusCode).to.be.equal(200);
+      expect(body).to.be.equal('Welcome to the payment system');
       done();
     });
   });
