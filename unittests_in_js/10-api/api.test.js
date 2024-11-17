22a6dbdba4921d2d6cce21bb6dc19ccfdb62c6b9
melekmoalla <melekmoalla2468@gmail.com>
2024-11-17 18:44:46 +0100

    test

--- unittests_in_js/10-api/api.test.js
+++ unittests_in_js/10-api/api.test.js
@@ -13,30 +13,36 @@ describe('Index page', () => {
 
   it('should return the correct result for GET /', (done) => {
     request.get(`${baseUrl}/`, (err, res, body) => {
-    expect(res.statusCode).to.equal(200);
+      expect(res.statusCode).to.equal(200);
       expect(body).to.equal('Welcome to the payment system');
       done();
     });
   });
 
-  it('should return the correct result for GET /', (done) => {
-    request.get(`${baseUrl}/available_payments`, {json: {userName: 'Betty'}}, (err, res, body) => {
-    expect(res.statusCode).to.equal(200);
-      expect(body).to.equal({payment_methods: {
-        credit_cards: true,
-        paypal: false,
-      }});
+  it('should return the correct result for GET /available_payments', (done) => {
+    request.get(`${baseUrl}/available_payments`, (err, res, body) => {
+      expect(res.statusCode).to.equal(200);
+      expect(JSON.parse(body)).to.deep.equal({
+        payment_methods: {
+          credit_cards: true,
+          paypal: false,
+        },
+      });
       done();
     });
   });
 
-  it('should return the correct result for GET /', (done) => {
-    request.get(`${baseUrl}/login`, {json: {userName: 'Betty'}}, (err, res, body) => {
-    expect(res.statusCode).to.equal(200);
-      expect(body).to.equal('Welcome Betty');
-      done();
-    });
+  it('should return the correct result for POST /login', (done) => {
+    request.post(
+      {
+        url: `${baseUrl}/login`,
+        json: { userName: 'Betty' },
+      },
+      (err, res, body) => {
+        expect(res.statusCode).to.equal(200);
+        expect(body).to.equal('Welcome Betty');
+        done();
+      }
+    );
   });
-
-
 });
