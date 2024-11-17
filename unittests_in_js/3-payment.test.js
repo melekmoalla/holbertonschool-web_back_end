c30eb53e1bfa5e8d23376c5b66c98d4c1625c527
melekmoalla <melekmoalla2468@gmail.com>
2024-11-17 11:34:11 +0100

    test

--- unittests_in_js/3-payment.test.js
+++ unittests_in_js/3-payment.test.js
@@ -1,52 +1,16 @@
+
 const sinon = require('sinon');
 const { expect } = require('chai');
 const Utils = require('./utils');
 const sendPaymentRequestToApi = require('./3-payment');
 
+
 describe('sendPaymentRequestToApi', () => {
-  let spy;
-
-  beforeEach(() => {
-    // Set up the spy before each test
-    spy = sinon.spy(Utils, 'calculateNumber');
+    it('should use Utils.calculateNumber with the correct arguments', () => {
+        const spy = sinon.spy(Utils, 'calculateNumber');
+  
+        sendPaymentRequestToApi(100, 20);
+        expect(spy.calledWith('SUM', 100, 20)).to.be.true;
+        spy.restore();
+    });
   });
-
-  afterEach(() => {
-    // Restore the original method after each test
-    spy.restore();
-  });
-
-  it('should call Utils.calculateNumber with SUM, 100, 20', () => {
-    sendPaymentRequestToApi(100, 20);
-
-    expect(spy.calledOnceWithExactly('SUM', 100, 20)).to.be.true;
-  });
-
-  it('should call Utils.calculateNumber with SUM, 10.5, 9.4', () => {
-    sendPaymentRequestToApi(10.5, 9.4);
-
-    expect(spy.calledOnceWithExactly('SUM', 10.5, 9.4)).to.be.true;
-  });
-
-  it('should log the correct total when called', () => {
-    const consoleSpy = sinon.spy(console, 'log');
-
-    sendPaymentRequestToApi(100, 20);
-
-    expect(consoleSpy.calledOnceWithExactly('The total is: 120')).to.be.true;
-
-    consoleSpy.restore();
-  });
-
-  it('should call calculateNumber only once', () => {
-    sendPaymentRequestToApi(30, 40);
-
-    expect(spy.calledOnce).to.be.true;
-  });
-
-  it('should work with negative numbers', () => {
-    sendPaymentRequestToApi(-10, -20);
-
-    expect(spy.calledOnceWithExactly('SUM', -10, -20)).to.be.true;
-  });
-});
